ac01a2969781b960a14e0dcc39d4b223e2b2e5c9
Aaren Vo <aarenjvo@gmail.com>
2024-03-05 16:22:43 -0800

    Adding 404 page view

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    res.status(404).render('error404')
 })
 
 const PORT = process.env.PORT || 3000
