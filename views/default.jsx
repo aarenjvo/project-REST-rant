4815de5fb5385807f5df44a96d6a8a67d9c5e0c1
Aaren Vo <aarenjvo@gmail.com>
2024-03-09 10:38:17 -0800

    Navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -1,7 +1,7 @@
 const React = require('react')
 
 function Def(html) {
-    return(
+    return (
         <html>
             <head>
                 <title>Title</title>
@@ -9,6 +9,19 @@ function Def(html) {
                 <link rel="stylesheet" href="/style.css" />
             </head>
             <body>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/places">Places</a>
+                        </li>
+                        <li>
+                            <a href="/places/new">Add Place</a>
+                        </li>
+                    </ul>
+                </nav>
                 {html.children}
             </body>
         </html>
